02c10f103a881fa8561670b37341e7bd23cccb6e
lossp <richard.xxmxx@gmail.com>
2017-09-14 15:02:20 +0800

    登陆注册注销功能

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 import Vue from 'vue';
+import AV from 'leancloud-storage'
+var APP_ID = '{{appid}}';
+var APP_KEY = '{{appkey}}';
+
+AV.init({
+  appId: 'Dcb3bYP5lI1MAg38kp68pJij-9Nh9j0Va',
+  appKey: 'CrEeVYrvduiiAJjkHpA4mNSQ'
+});
 
 var app = new Vue({
   el: '#app',
   data: {
     newTodo: '',
-    todoList: []
+    todoList: [],
+    actionType: 'signUp',
+    formData:{
+      username:'',
+      password:''
+    },
+    currentUser:null
   },
   created: function(){
     window.onbeforeunload=()=>{
@@ -14,6 +28,7 @@ var app = new Vue({
     let oldDataString = window.localStorage.getItem('myTodos')
     let oldData = JSON.parse(oldDataString)
     this.todoList = oldData || []
+    this.currentUser = this.getCurrentUser()
   },
   methods:{
     addTodo: function(){
@@ -27,6 +42,38 @@ var app = new Vue({
     removeTodo: function(todo){
       let index = this.todoList.indexOf(todo);
       this.todoList.splice(index, 1);
+    },
+    signUp: function(){
+      console.log(213)
+      let user = new AV.User();
+      user.setUsername(this.formData.username);
+      user.setPassword(this.formData.password);
+      user.signUp().then((loginedUser) => {
+        this.currentUser = this.getCurrentUser()
+      }, (error) => {
+        alert('注册失败')
+      });
+    },
+    login:function(){
+      AV.User.logIn(this.formData.username, this.formData.password).then((loginedUser) => {
+        this.currentUser = this.getCurrentUser()
+      },function(err){
+        alert('登陆失败')
+      })
+    },
+    getCurrentUser: function(){
+      let current = AV.User.current();
+      if(current){
+        let{id, createdAt, attributes:{username}} = AV.User.current();
+        return{id, createdAt, username};
+      }else{
+        return null;
+      }
+    },
+    logout: function(){
+      AV.User.logOut();
+      this.currentUser = null;
+      window.location.reload();
     }
   }
-})        
+});      
