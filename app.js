ba2ec558f453cb78965173e8c6ba9a9e003fe4c5
lossp <richard.xxmxx@gmail.com>
2017-09-18 07:53:34 +0800

    重做，实现了增删功能，标记完成功能，localStorage功能

--- app.js
+++ app.js
@@ -9,80 +9,33 @@ AV.init({
 });
 
 var app = new Vue({
-  el: '#app',
-  data: {
-    newTodo: '',
-    todoList: [],
-    actionType: 'signUp',
-    formData:{
-      username:'',
-      password:''
+    el:'#app',
+    data:{
+        newTodo:'',
+        todoList:[]
     },
-    currentUser:null
-  },
-  created: function(){
-    this.currentUser = this.getCurrentUser()
-  },
-  methods:{
-    saveTodos:function(){
-      let dataString = JSON.stringify(this.todoList) 
-      var AVTodos = AV.Object.extend('AllTodos');
-      var avTodos = new AVTodos();
-      avTodos.set('content', dataString);
-      avTodos.save().then(function (todo) {
-        // 成功保存之后，执行其他逻辑.
-        console.log('保存成功');
-      }, function (error) {
-        // 异常处理
-        console.error('保存失败');
-      })
-     },
-    addTodo: function(){
-      this.todoList.push({
-        title: this.newTodo,
-        createdAt: new Date(),
-        done: false
-      })
-      this.newTodo='';
-      console.log(123)
-      this.saveTodos()
+    methods:{
+        addTodo:function(){
+            this.todoList.push({
+                title: this.newTodo,
+                createdAt: new Date(),
+                done: false
+            })
+            console.log(this.todoList);
+            this.newTodo = '';
+        },
+        deleteTodo:function(todo){
+            let index = this.todoList.indexOf(todo);
+            this.todoList.splice(index,1);
+        }
     },
-    removeTodo: function(todo){
-      let index = this.todoList.indexOf(todo);
-      this.todoList.splice(index, 1);
-      this.saveTodos();
-    },
-    signUp: function(){
-      console.log(213)
-      let user = new AV.User();
-      user.setUsername(this.formData.username);
-      user.setPassword(this.formData.password);
-      user.signUp().then((loginedUser) => {
-        this.currentUser = this.getCurrentUser()
-      }, (error) => {
-        alert('注册失败')
-      });
-    },
-    login:function(){
-      AV.User.logIn(this.formData.username, this.formData.password).then((loginedUser) => {
-        this.currentUser = this.getCurrentUser()
-      },function(err){
-        alert('登陆失败')
-      })
-    },
-    getCurrentUser: function(){
-      let current = AV.User.current();
-      if(current){
-        let{id, createdAt, attributes:{username}} = AV.User.current();
-        return{id, createdAt, username};
-      }else{
-        return null;
-      }
-    },
-    logout: function(){
-      AV.User.logOut();
-      this.currentUser = null;
-      window.location.reload();
+    created:function(){
+        window.onbeforeunload=()=>{
+            let dataString = JSON.stringify(this.todoList);
+            window.localStorage.setItem('myTodos', dataString)
+        }
+        let oldDataString = window.localStorage.getItem('myTodos')
+        let oldData = JSON.parse(oldDataString)
+        this.todoList = oldData || []
     }
-  },
-})   
+})
